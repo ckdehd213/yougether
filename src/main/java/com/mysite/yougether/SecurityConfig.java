90756197c8baedf2534b1119595349661c4a2ada
changdongyu <ckdehd213@gmail.com>
2022-07-05 15:13:34 +0900

    next join template

--- src/main/java/com/mysite/yougether/SecurityConfig.java
+++ src/main/java/com/mysite/yougether/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.mysite.yougether;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +10,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
+import com.mysite.yougether.user.UserSecurityService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final UserSecurityService userSecurityService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/**").permitAll()
@@ -22,9 +30,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.headers()
 				.addHeaderWriter(new XFrameOptionsHeaderWriter(
 						XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+			.and()
+				.formLogin()
+				.loginPage("/user/login")
+				.defaultSuccessUrl("/")
 			;
 	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
--- src/main/java/com/mysite/yougether/user/UserController.java
+++ src/main/java/com/mysite/yougether/user/UserController.java
@@ -21,16 +21,16 @@ public class UserController {
 	@GetMapping("/signup")
 	public String signup(UserCreateForm userCreateForm) {
 		
-		return "/signup_form";
+		return "signup_form";
 	}
 	
 	@PostMapping("/signup")
 	public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
 		if(bindingResult.hasErrors()) {
-			return "/signup_form";
+			return "signup_form";
 		}if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){
 			bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
-			return "/signup_form";
+			return "signup_form";
 		}
 		try {
 		
@@ -46,6 +46,11 @@ public class UserController {
 			return "signup_form";
 		}
 		return "redirect:/";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login_form";
 	}
 }
 
